eb692edfc3ec03e8e4b70496e7bdda62adcd3d29
Arno Pan <apan11@my.bcit.ca>
2025-03-20 11:26:54 -0700

    add retrieving SQLITE3 by dated and not deleted

--- index.php
+++ index.php
@@ -24,6 +24,29 @@
             </ul>
         </div>
     </nav>
+    <div class="container mt-4">
+        <h1>Blog Posts</h1>
+        <?php
+        try{
+            $db = new SQLite3('data.db');
+            $resslts = $db->query('SELECT * FROM posts WHERE is_deleted = 0 ORDER BY created_at DESC');
+
+            while ($row = $results->fetchArray(SQLITE3_ASSOC)){
+                echo '<div class="card mb-3">';
+                echo '<div class="card-body">';
+                echo '<h5 class="card-title"></h5>';
+                echo '<p class="card-text"></p>';
+                echo '<p class="card-text"></p>';
+                echo '<a href="#" class="btn btn-primary">Read More</a>';
+                echo '</div>';
+                echo '</div>';
+
+            }
+        } catch (Exception $e) {
+            echo '<p>Error: ' . $e->getMessage() . '</p>';
+        }
+        ?>
+    </div>
     <script src="/script.js"></script>
 </body>
 </html>

01002c70ae1aca8749c37636a2a22b670bf55426
Arno Pan <apan11@my.bcit.ca>
2025-03-20 11:42:33 -0700

    displayed data from SQLITE3 server using PHP back-end

--- index.php
+++ index.php
@@ -29,19 +29,19 @@
         <?php
         try{
             $db = new SQLite3('data.db');
-            $resslts = $db->query('SELECT * FROM posts WHERE is_deleted = 0 ORDER BY created_at DESC');
+            $results = $db->query('SELECT * FROM posts WHERE is_deleted = 0 ORDER BY created_at DESC');
 
             while ($row = $results->fetchArray(SQLITE3_ASSOC)){
                 echo '<div class="card mb-3">';
                 echo '<div class="card-body">';
-                echo '<h5 class="card-title"></h5>';
-                echo '<p class="card-text"></p>';
-                echo '<p class="card-text"></p>';
+                echo '<h5 class="card-title">' . htmlspecialchars($row['title']) . '</h5>';
+                echo '<p class="card-text">' . htmlspecialchars($row['category']) . '</p>';
+                echo '<p class="card-text">' . substr(htmlspecialchars($row['content']), 0, 200) .'</p>';
                 echo '<a href="#" class="btn btn-primary">Read More</a>';
                 echo '</div>';
                 echo '</div>';
-
             }
+            $db->close();
         } catch (Exception $e) {
             echo '<p>Error: ' . $e->getMessage() . '</p>';
         }

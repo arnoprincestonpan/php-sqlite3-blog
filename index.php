e814a4730e52f5eb1d5d92b7390c9a6c99d81296
Arno Pan <apan11@my.bcit.ca>
2025-03-20 11:19:52 -0700

    navbar added - bootstrap styles

--- index.php
+++ index.php
@@ -8,6 +8,22 @@
     <link rel="stylesheet" href="/styles.css">
 </head>
 <body>
+    <nav class='navbar navbar-expand-lg navbar-light bg-light'>
+        <a class="navbar-brand" href="#">My Blog</a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="create.php">Create Post</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="trash.php">Trash</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <script src="/script.js"></script>
 </body>
 </html>

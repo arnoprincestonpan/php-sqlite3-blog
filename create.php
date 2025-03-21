ea584afea962c05cbd2e58e8a792e023dcc48bb5
Arno Pan <apan11@my.bcit.ca>
2025-03-21 00:46:14 -0700

    add form to create.php

--- create.php
+++ create.php
@@ -2,7 +2,29 @@
 $pageTitle = "Create Post";
 include "header.php"; 
 ?>
-    <div>
-
+    <div class="container mt-4">
+        <h1>Create Post</h1>
+        <form action="">
+            <div>
+                <label>Title</label>
+                <input type="text" id="title" name="title" />
+            </div>
+            <div>
+                <label>Category</label>
+                <input type="text" id="title" name="title" />
+            </div>
+            <div>
+                <label>Content Type</label>
+                <select>
+                    <option value="rich">Rich Text</option>
+                    <option value="markdown">Markdown</option>
+                </select>
+            </div>
+            <div>
+                <label>Content</label>
+                <input type="text" id="title" name="title" />
+            </div>
+            <button>Submit</button>
+        </form>
     </div>
 <?php include "footer.php"; ?>

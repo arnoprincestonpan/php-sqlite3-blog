e9d160a230318bd8d71d6b785aaf8553064b390d
arnoprincestonpan <pan.arno@gmail.com>
2025-03-24 16:32:15 -0700

    try prepare statementds. Bind values to prevent SQL injection. Catch Exception with Error message.

--- process_post.php
+++ process_post.php
@@ -5,7 +5,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $content_type = $_POST['content_type'];
     $category = $_POST['category'];
 
-    
+    try {
+        $db = new SQLite3('data.db');
+        $stmt = $db->prepare('INSERT INTO posts (title, content, content_type, category) VALUES (:title, :content, :content_type, :category)');
+        $stmt->bindValue(':title', $title, SQLITE3_TEXT);
+        $stmt->bindValue(':content', $content, SQLITE3_TEXT);
+        $stmt->bindValue(':content_type', $content_type, SQLITE3_TEXT);
+        $stmt->bindValue(':category', $category, $SQLITE3_TEXT);
+        $stmt->execute();
+        $db->close();
+        header('Location: index.php');
+        exit;
+    } catch (Exception $e){
+        echo "<p>/Error: " . $e->getMessage() . "</p>";
+    }
 }
-
 ?>

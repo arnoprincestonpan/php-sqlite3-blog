f97b3ef3f8698be5b7d894c1d4297a2aa3b54678
arnoprincestonpan <pan.arno@gmail.com>
2025-03-24 16:26:03 -0700

    start process_post.php

---
+++ process_post.php
@@ -0,0 +1,11 @@
+<?php 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $content_type = $_POST['content_type'];
+    $category = $_POST['category'];
+
+    
+}
+
+?>
